5b309526feb4b7d2b19eac58f841e3aa3a19911d
pnagre05 <90118755+pnagre05@users.noreply.github.com>
2025-05-24 12:27:56 -0500

    Update staticMembers.cpp

--- chapterFive/staticMembers.cpp
+++ chapterFive/staticMembers.cpp
@@ -20,6 +20,9 @@ class Circle
 		}
 };
 
+//static members are declared within the class but defined outside of it
+//you need both parts - declaration and definition otherwise there will be a linker error
+
 int Circle::count = 0;
 
 int main() 
@@ -28,4 +31,14 @@ int main()
 	Circle::showCount();
 	Circle c2('B', 2.2f);
 	Circle::showCount();
+
+	/* 
+ 	An instance member (like a constructor) can access instance as well as static data members
+
+   	A static member function can access only static data members
+
+    	A this pointer never exists for a static function
+
+     	A static data member has nothing to do with static storage class
+ */
 }
