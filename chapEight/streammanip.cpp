cc92435ae1e56a19c7baaa4e4b1b2d6d7f8f4d5c
pnagre05 <90118755+pnagre05@users.noreply.github.com>
2025-05-29 17:11:54 -0500

    Create streammanip.cpp

---
+++ chapEight/streammanip.cpp
@@ -0,0 +1,40 @@
+    #include <iostream>
+    #include <iomanip>
+
+    using namespace std;
+
+    int main()
+    {
+        int i = 752;
+        float a = 425;
+        float b = 123.500328f;
+        char str[] = "Dream. Then make it happen!";
+
+        ios_base::fmtflags oldFlags;
+        oldFlags = cout.flags();
+        cout << "Current flags = " << cout.flags() << endl;
+
+        cout << hex << i << endl;
+        cout << showbase << hex << i << endl;
+        cout << uppercase << showbase << hex << i << endl;
+        cout << dec << i << endl;
+        cout << internal << showpos << setw(10) << i << endl;
+        cout << i << endl;
+
+        cout << setfill('0');
+        cout << "Fill character: " << cout.fill() << endl;
+        cout << setw(40) << str << endl;
+        cout << left << setw(40) << str << endl;
+
+        cout.precision(6);
+        cout << "Precision: " << cout.precision() << endl;
+        cout << showpoint << showpos << a << endl;
+        cout << fixed << b << endl;
+        cout << scientific << b << endl;
+
+        cout << "Current flags = " << cout.flags() << endl;
+        cout.flags(oldFlags);
+        cout << "Current flags = " << cout.flags() << endl;
+        return 0;
+
+    }
